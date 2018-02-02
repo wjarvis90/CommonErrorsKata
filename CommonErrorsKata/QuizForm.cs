a56962f5956b63398743cc83a4163f1eae55ca7e
Wes Jarvis <wes.b.jarvis@gmail.com>
2018-02-02 08:29:58 -0600

    Refactored QuizForm

--- CommonErrorsKata/QuizForm.cs
+++ CommonErrorsKata/QuizForm.cs
@@ -16,16 +16,16 @@ namespace CommonErrorsKata
         private int _time = 100;
         private string _visibleImagePath = null;
         private readonly string[] _possibleAnswers = null;
+        private readonly int _maxAnswers = 15;
 
         public CommonErrorsForm()
         {
             InitializeComponent();
             _synchronizationContext = SynchronizationContext.Current;
             _files = Directory.GetFiles(Environment.CurrentDirectory + @"..\..\..\ErrorPics");
-            _possibleAnswers = new[] { "Missing File", "null instance", "divide by zero" };
             _possibleAnswers = _files.Select(f => Path.GetFileName(f)?.Replace(".png", " ")).ToArray();
             lstAnswers.DataSource = _possibleAnswers;
-            _answerQueue = new AnswerQueue<TrueFalseAnswer>(15);
+            _answerQueue = new AnswerQueue<TrueFalseAnswer>(_maxAnswers);
             Next();
             lstAnswers.Click += LstAnswers_Click;
             StartTimer();
@@ -47,31 +47,23 @@ namespace CommonErrorsKata
         {
             _time = 100;
             var selected = _possibleAnswers[lstAnswers.SelectedIndex];
-            if (selected != null && selected == _visibleImagePath)
-            {
-                _answerQueue.Enqueue(new TrueFalseAnswer(true));
-            }
-            else
-            {
-                _answerQueue.Enqueue(new TrueFalseAnswer(false));
-            }
-            var tokens = _visibleImagePath.Split(' ');
-            _answerQueue.Enqueue(new TrueFalseAnswer(true));
+            var isCorrect = selected != null && selected == _visibleImagePath;
+            _answerQueue.Enqueue(new TrueFalseAnswer(isCorrect));
+
             Next();
         }
 
         private void Next()
         {
-            if (_answerQueue.Count == 4 && _answerQueue.Grade >= 98)
+            if (_answerQueue.Count == _maxAnswers && _answerQueue.Grade >= 98)
             {
                 MessageBox.Show("Congratulations you've defeated me!");
                 Application.Exit();
                 return;
             }
-            label1.Text = _answerQueue.Grade.ToString() + "%";
+            label1.Text = _answerQueue.Grade + "%";
             var file = _files.GetRandom();
             _visibleImagePath = Path.GetFileName(file)?.Replace(".png", " ");
-            _visibleImagePath = Path.GetFileName(file);
             pbImage.ImageLocation = file;
         }
 
